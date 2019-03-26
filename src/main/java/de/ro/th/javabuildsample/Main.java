a382f13a087f2aa5102a339c53567d9302788e15
Thomas Mildner <thomas.mildner@media-concept.com>
2019-03-26 10:16:29 +0100

    add unit tests and calculator method

--- src/main/java/de/ro/th/javabuildsample/Main.java
+++ src/main/java/de/ro/th/javabuildsample/Main.java
@@ -2,7 +2,12 @@ package de.ro.th.javabuildsample;
 
 public class Main
 {
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
+        System.out.println("Testing Calculator - Demo Program");
+        System.out.println(String.format("1 + 4 = %s", Calculator.add(1,4)));
 
+
+    }
 
 }
---
+++ src/test/java/TestCalculator.java
@@ -0,0 +1,18 @@
+import de.ro.th.javabuildsample.Calculator;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestCalculator
+{
+    @Test
+    public void testAdd(){
+       int result = Calculator.add(2,3);
+        Assert.assertEquals(5, result);
+    }
+
+    @Test
+    public void testMultiply(){
+    int result = Calculator.multiply(1,4);
+    Assert.assertEquals(4, result);
+    }
+}
