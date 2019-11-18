0c37124f7ae7edc60c8fea4476d986f4a590256d
Thomas Mildner <jonny9904@googlemail.com>
2019-11-18 18:32:28 +0100

    test commit

--- src/main/java/de/ro/th/javabuildsample/Main.java
+++ src/main/java/de/ro/th/javabuildsample/Main.java
@@ -7,7 +7,9 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Testing Calculator - Demo Program");
-        System.out.println(String.format("1 + 4 = %s", Calculator.add(1,4)));
+        System.out.println(String.format("1 + 4 = %s", Calculator.add(1, 4)));
+
+        
 
 
     }
